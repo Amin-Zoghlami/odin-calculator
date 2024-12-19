37e9e79e8057174d719939482b2c7792b6d08670
Amin Zoghlami <amin.zoghlami.361@my.csun.edu>
2024-12-19 02:12:54 -0800

    Add CSS and JS

--- script.js
+++ script.js
@@ -1,46 +1,118 @@
-function add(num1, num2) {
-    num1 = Number(num1);
-    num2 = Number(num2);
-    return num1 + num2;
+function add(a, b) {
+    a = Number(a);
+    b = Number(b);
+    return a + b;
 }
 
-function subtract(num1, num2) {
-    num1 = Number(num1);
-    num2 = Number(num2);
-    return num1 - num2;
+function subtract(a, b) {
+    a = Number(a);
+    b = Number(b);
+    return a - b;
 }
 
-function multiply(num1, num2) {
-    num1 = Number(num1);
-    num2 = Number(num2);
-    return num1 * num2;
+function multiply(a, b) {
+    a = Number(a);
+    b = Number(b);
+    return a * b;
 }
 
-function divide(num1, num2) {
-    num1 = Number(num1);
-    num2 = Number(num2);
-    if (num2 === 0) {
+function divide(a, b) {
+    a = Number(a);
+    b = Number(b);
+    if (b === 0) {
         return "ERROR";
     }
-    return num1 / num2;
+    return a / b;
 }
 
-function operate(num1, num2, operator) {
+function clearAll() {
+    display.textContent = "";
+    displayNumber = "";
+    previousNumber = "";
+    operator = '';
+}
+
+function changeDisplay() {
+    display.textContent = displayNumber;
+}
+
+function operateDisplay(a, b, operator) {
+    if (previousNumber === ""
+    || displayNumber === ""
+    || currentOperator === "") {
+        return;
+    }
+
     if (operator === '+') {
-        return add(num1, num2);
+        displayNumber = add(a, b);
+        changeDisplay();
     }
     if (operator === '-') {
-        return subtract(num1, num2);
+        displayNumber = subtract(a, b);
+        changeDisplay();
     } 
     if (operator === '*') {
-        return multiply(num1, num2);
+        displayNumber = multiply(a, b);
+        changeDisplay();
     } 
     if (operator === '/') {
-        return divide(num1, num2);
+        displayNumber = divide(a, b);
+        changeDisplay();
+        if (displayNumber === "ERROR") {
+            displayNumber = "";
+        }
     }
 }
 
-console.log(operate(1, 3, '/'));
-let num1 = "";
-let num2 = "";
-let operator= '';
+let displayNumber = "";
+let previousNumber = "";
+let currentOperator= '';
+
+const display = document.querySelector(".display");
+
+for (let i = 0; i < 10; i++) {
+    const numberButton = document.querySelector(`.button${i}`);
+    numberButton.addEventListener("click", () => {
+        displayNumber += `${i}`;
+        changeDisplay(`${i}`);
+    });
+}
+
+const clearButton = document.querySelector(".clear");
+clearButton.addEventListener("click", () => clearAll());
+
+const equalButton = document.querySelector(".equal");
+equalButton.addEventListener("click", () => 
+    operateDisplay(previousNumber, displayNumber, currentOperator));
+
+const addButton = document.querySelector(".add");
+addButton.addEventListener("click", () => {
+    operateDisplay(previousNumber, displayNumber, currentOperator);
+    previousNumber = displayNumber;
+    displayNumber = "";
+    currentOperator = '+';
+});
+
+const subtractButton = document.querySelector(".subtract");
+subtractButton.addEventListener("click", () => {
+    operateDisplay(previousNumber, displayNumber, currentOperator);
+    previousNumber = displayNumber;
+    displayNumber = "";
+    currentOperator = '-';
+});
+
+const multiplyButton = document.querySelector(".multiply");
+multiplyButton.addEventListener("click", () => {
+    operateDisplay(previousNumber, displayNumber, currentOperator);
+    previousNumber = displayNumber;
+    displayNumber = "";
+    currentOperator = '*';
+});
+
+const divideButton = document.querySelector(".divide");
+divideButton.addEventListener("click", () => {
+    operateDisplay(previousNumber, displayNumber, currentOperator);
+    previousNumber = displayNumber;
+    displayNumber = "";
+    currentOperator = '/';
+});
